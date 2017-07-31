a49b15471809add99ee33737b53a15e12cf8c6e5
sarleon <sarleon56@hotmail.com>
2017-07-31 14:18:17 +0800

    add base structure

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,14 +1,20 @@
+#coding:utf-8
 import commands,re
 from output_helper import color_print,debug_log
 class BaseModule():
     def __init__(self,requirement):
-        self.requirement= requirement
 
+        self.requirement= requirement
+        self.name = "Base Module"
+
+    """
+    模块运行之前的检查
+    """
     def pre_check(self):
 
         if self.requirement !=None:
             if not self.exist():
-                color_print("system command "+self.requirement+" not found ,please check if the requirement is installed",1)
+                color_print("["+self.name+"]"+"system command "+self.requirement+" not found ,please check if the requirement is installed",1)
                 return False
 
 
--- output_helper.py
+++ output_helper.py
@@ -1,29 +1,34 @@
 import time
 
-
 debug = True
+
 
 def debug_log(content):
     if debug:
         print content
 
+
 def common_print(content):
     print  content
-def color_print(content,color_offset):
-    BASE         = '\033[0;3%dm'%color_offset
-    END ='\033[0m'
-    common_print(BASE+content+END)
+
+
+def color_print(content, color_offset):
+    BASE = '\033[0;3%dm' % color_offset
+    END = '\033[0m'
+    common_print(BASE + content + END)
+
 
 def time_print(content):
     common_print(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()) + str(content))
 
+
 def print_banner():
-    banner="""
+    banner = """
 _____________      _____                         ____________                         _______              ______              _____
 ___  __ \__(_)_______  /____  _____________      __  ___/_  /____________ ______      ___    |____________ ___  /____  ___________(_)_______    {0.0.1#unstable}
 __  /_/ /_  /_  ___/  __/  / / /_  ___/  _ \     _____ \_  __/  _ \_  __ `/  __ \     __  /| |_  __ \  __ `/_  /__  / / /_  ___/_  /__  ___/
-_  ____/_  / / /__ / /_ / /_/ /_  /   /  __/     ____/ // /_ /  __/  /_/ // /_/ /     _  ___ |  / / / /_/ /_  / _  /_/ /_(__  )_  / _(__  )
-/_/     /_/  \___/ \__/ \__,_/ /_/    \___/      /____/ \__/ \___/_\__, / \____/      /_/  |_/_/ /_/\__,_/ /_/  _\__, / /____/ /_/  /____/
+_  ____/_  / / /__ / /_ / /_/ /_  /   /  __/     ____/ // /_ /  __/  /_/ // /_/ /     _  ___ |  / / / /_/ /_  / _  /_/ /_(__  )_  / _(__  )                           author : centurio
+/_/     /_/  \___/ \__/ \__,_/ /_/    \___/      /____/ \__/ \___/_\__, / \____/      /_/  |_/_/ /_/\__,_/ /_/  _\__, / /____/ /_/  /____/               License: GPLv3
                                                                   /____/                                        /____/
     """
-    color_print(banner,4)
+    color_print(banner, 4)
