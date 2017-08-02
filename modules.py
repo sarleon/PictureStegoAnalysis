2b3122a8745ccb8a125e2e774253f52e2b7358e8
sarleon <sarleon56@hotmail.com>
2017-08-02 17:49:16 +0800

    complele foremost auxiliary module

--- modules.py
+++ modules.py
@@ -40,7 +40,7 @@ class BaseModule():
 
         if self.requirement !=None:
             if not self.exist():
-                color_print("["+self.name+"]"+"system command "+self.requirement+" not found ,please check if the requirement is installed",1)
+                color_print("["+self.name+"]"+"软件 "+self.requirement+"未找到或添加到环境变量中,",1)
                 return False
 
 
@@ -61,7 +61,10 @@ class BaseModule():
     def run(self,filename,content=None):
         raise NotImplementedError
 
-
+class FoundementalCheckModule(BaseModule):
+    requirement = None
+    name = "基本检查"
+    desribtion = "一些基本的检查"
 
 
 
@@ -90,9 +93,39 @@ class ForemostModule(BaseModule):
 
 
         result = commands.getoutput("foremost -o "+tmp_path+" "+filename )
+        tmp_files = os.listdir(tmp_path)
+        file_type_num = len(tmp_files) - 1
+        total_files = []
+        for diretory in tmp_files:
+            if diretory != 'audit.txt':
+                dir_path = tmp_path+diretory
+                sub_dir_files = os.listdir(dir_path)
+                map(total_files.append,[dir_path+os.path.sep+sub_dir_file for sub_dir_file in sub_dir_files])
+        n = len(total_files)
+        if n>1:
+            self.module_print("共分离出了%d个文件,文件名为"%n)
+            for file_path in total_files:
+                os.system("mv %s PSA-Foremost-%s"%(file_path,file_path))
+                color_print("PSA-Foremost-%s"%file_path,5)
+        else:
+            self.module_print("仅分离出一个文件")
+            origin_size = os.path.getsize(filename)
+            self.module_print("源文件的大小为%d"%origin_size)
+            cur_size = os.path.getsize(total_files[0])
+            self.module_print("分离出的文件的大小为%d"%cur_size)
+            if origin_size>cur_size:
+                self.module_print("源文件的大小大于分离出的文件的大小,源文件中可能存在隐藏信息")
+            f_origin = open(filename,'rb')
+            f_origin.seek(cur_size)
+            data = f_origin.read()
+            self.module_print("从尾部提取出的可能的隐藏信息是")
+            color_print("********************************上分割线**********************************",6)
+            color_print(data,4)
+            color_print("********************************下分割线**********************************",6)
 
 
         commands.getoutput('rm -rf '+tmp_path)
+
 
 
 
